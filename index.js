19388efe7c3aab3cd322664e6e924539c2e18c57
yugeshcoder <yugesh.v.1013@gmail.com>
2024-08-11 12:35:49 +0530

    done

--- index.js
+++ index.js
@@ -337,6 +337,9 @@ function calc(){
   document.getElementById("3f3").textContent = `${Math.round(((t18+t19+t20+t21)/showtotal)*100)}%`;
   document.getElementById("2f3").textContent = `${Math.round(((t22+t23+t24+t25)/showtotal)*100)}%`;
 
+  document.getElementById("765tp").textContent = `${Math.round(((t26+t27+t28+t29+t1+t2+t3+t4+t5+t6+t7+t8+t9+t10+t11+t12+t13)/showtotal)*100)}%`;
+  document.getElementById("432tp").textContent = `${Math.round(((t14+t15+t16+t17+t18+t19+t20+t21+t22+t23+t24+t25)/showtotal)*100)}%`;
+
   tp7 = p26+p27+p28+p29+p1+p2+p3+p4+p5;
   tp6 = p6+p7+p8+p9;
   tp5 = p10+p11+p12+p13;
